64bc35be64b27434f1f3ae5b4a48c9943972a5a3
vasudev93t <33799630+vasudev93t@users.noreply.github.com>
2022-02-17 22:09:27 -0600

    Add files via upload

---
+++ csv_combiner.py
@@ -0,0 +1,64 @@
+import sys
+import os
+import pandas as pd
+from dataclasses import dataclass
+
+
+class CSV_Combiner:
+
+    @staticmethod
+    def check_file_paths(userinput):
+
+        if len(userinput) <= 1:
+            print("Error: No file-paths input. Please run the code as follows: \n" +
+                  "python ./csv_combiner.py ./fixtures/accessories.csv ./fixtures/clothing.csv > combined.csv")
+            return False
+
+        filelst = userinput[1:]
+
+        for file_path in filelst:
+            if os.stat(file_path).st_size == 0:
+                print("File is empty: " + file_path)
+                return False
+            if not os.path.exists(file_path):
+                print("Not found: " + file_path)
+                return False
+        return True
+
+    def combine_files(self, userinput: list):
+
+        chunksize = 100000  # Reading 100000 rows at once
+        rows_list = []
+
+        if self.check_file_paths(userinput):
+            filelst = userinput[1:]
+
+            for file_path in filelst:
+
+                # chunksize option in pandas to ready in batches
+                for rows in pd.read_csv(file_path, chunksize=chunksize):
+
+                    filename = os.path.basename(file_path)
+
+                    # creating now column with respective filename for rows
+                    rows['filename'] = filename
+                    rows_list.append(rows)
+
+            header = True
+
+            # combine all chunks
+            for row in rows_list:
+                print(row.to_csv(index=False, header=header,
+                      line_terminator='\n', chunksize=chunksize), end='')
+                header = False  # Only needed for the first file
+        else:
+            return
+
+
+def main():
+    combiner = CSVCombiner()
+    combiner.combine_files(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
